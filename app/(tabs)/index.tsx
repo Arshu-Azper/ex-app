b77566d14987a474403c1999b48caa0b35384111
Arshu-Azper <kitcat.tuley@gmail.com>
2024-09-16 18:21:09 -0500

    made login page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,150 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {
+  Text,
+  TextInput,
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
 
 export default function HomeScreen() {
+    //Inputs
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    //Other Variables
+    const [isLoggingIn, setIsLoggingIn] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [loginHeader, setLoginHeader] = useState('Sign Up');
+  
+    const [welcome, setWelcome] = useState('');
+  
+    const goToLogin = () => {
+      setLoginHeader('Login');
+      setIsLoggingIn(true);
+    };
+    const goToSignUp = () => {
+      setLoginHeader('Sign Up');
+      setIsLoggingIn(false);
+    };
+  
+    const loggingIn = () => {
+      setLoggedIn(true);
+      if (isLoggingIn == false) {
+        setWelcome(
+          `Welcome ${firstName}, ${lastName}. Remember your login is your email (${email}) and password (${password})`
+        );
+      } else {
+        setWelcome(`Logged in with email (${email}) and password (${password})`);
+      }
+    };
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <SafeAreaView style={styles.container}>
+    {!loggedIn && (
+      <View style={styles.loginContainer}>
+        <Text style={{ color: 'rgb(33, 150, 243)', fontSize: 60 }}>
+          {loginHeader}
+        </Text>
+
+        {!isLoggingIn && (
+          <View style={styles.name}>
+            <TextInput
+              style={styles.inputs}
+              onChangeText={(text) => setFirstName(text)}
+              value={firstName}
+              autoCapitalize={'words'}
+              placeholder="First Name"
+            />
+
+            <TextInput
+              style={styles.inputs}
+              onChangeText={(text) => setLastName(text)}
+              value={lastName}
+              autoCapitalize={'words'}
+              placeholder="Last Name"
+            />
+          </View>
+        )}
+
+        <TextInput
+          style={styles.inputs}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          autoCapitalize={'words'}
+          placeholder="Enter Your Email"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+
+        <TextInput
+          style={styles.inputs}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          autoCapitalize={'words'}
+          placeholder="Enter Your Password"
+        />
+
+        <View style={styles.button}>
+          <Button title="Confirm" onPress={loggingIn} />
+        </View>
+
+        {!isLoggingIn && (
+          <>
+            <Text>Already have an account?</Text>
+            <Button title="Login" onPress={goToLogin} />
+          </>
+        )}
+        {isLoggingIn && (
+          <>
+            <Text>Don't have an account?</Text>
+            <Button title="Sign Up" onPress={goToSignUp} />
+          </>
+        )}
+      </View>
+    )}
+    {loggedIn && (
+      <View style={styles.loginContainer}>
+        <Text style={{ color: 'rgb(33, 150, 243)', fontSize: 60 }}>
+          Welcome{isLoggingIn && <Text> Back</Text>}
+        </Text>
+        <Text>{welcome}</Text>
+      </View>
+    )}
+  </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'gray',
+    padding: 8,
+    margin: 10,
+  },
+  loginContainer: {
+    gap: 10,
+    padding: 25,
+    margin: '5%',
+    backgroundColor: '#ecf0f1',
     alignItems: 'center',
-    gap: 8,
+    borderRadius: 16,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  name: {
+    width: '100%',
+    gap: 10,
+    alignItems: 'center',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  inputs: {
+    height: 40,
+    backgroundColor: 'white',
+    borderColor: 'rgb(33, 150, 243)',
+    borderWidth: 1,
+    borderRadius: 8,
+    textAlign: 'center',
+    width: '70%',
+  },
+  button: {
+    width: '80%',
   },
 });
